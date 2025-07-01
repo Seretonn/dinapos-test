64c581479058dd1d9880eed314e95348916b6d03
angel <angelcolinaalvarez@gmail.com>
2025-07-01 06:19:02 -0400

    minor fixes to view users.edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <form action="{{ route('admin.users.update', $user) }}" method="post">
         @csrf
-        @method("patch")
+        @method("PATCH")
         
         <h2>Editing User</h2>
         <label for="name">Name:</label>
@@ -11,14 +11,14 @@
         <input type="email" name="email" value="{{ old('email', $user->email) }}" required>
 
         <label for="password">New Password:</label>
-        <input type="password" name="password" required>
+        <input type="password" name="password">
 
         <label for="password_confirmation">Confirm New Password:</label>
-        <input type="password" name="password_confirmation" required>
+        <input type="password" name="password_confirmation">
 
         <div class="btn-container">
             <button type="submit" class="edit-btn mt-4">Update</button>
-            <a href="{{ route('tasks.index') }}" class="cancel-btn mt-4">Cancel</a>    
+            <a href="{{ route('admin.users.index') }}" class="cancel-btn mt-4">Cancel</a>    
         </div>
     </form>
 
